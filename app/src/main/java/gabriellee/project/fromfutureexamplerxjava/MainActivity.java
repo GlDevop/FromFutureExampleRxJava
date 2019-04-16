797e37c05811d0e36cc7b5221a8084bf20a865cb
GabeLeeData <gabe95sk@gmail.com>
2019-04-16 19:11:31 -0400

    Testing.

--- app/src/main/java/gabriellee/project/fromfutureexamplerxjava/MainActivity.java
+++ app/src/main/java/gabriellee/project/fromfutureexamplerxjava/MainActivity.java
@@ -1,13 +1,61 @@
 package gabriellee.project.fromfutureexamplerxjava;
 
+import android.arch.lifecycle.ViewModelProviders;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
+import okhttp3.ResponseBody;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        MainViewModel viewModel = ViewModelProviders.of(this).get(MainViewModel.class);
+        try {
+            viewModel.makeFutureQuery().get()
+                    .subscribeOn(Schedulers.io())
+                    .observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(new Observer<ResponseBody>() {
+                        @Override
+                        public void onSubscribe(Disposable d) {
+                            Log.d(TAG, "onSubscribe: called.");
+                        }
+
+                        @Override
+                        public void onNext(ResponseBody responseBody) {
+                            Log.d(TAG, "onNext: got the response from server!");
+                            try {
+                                Log.d(TAG, "onNext: " + responseBody.string());
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                        }
+
+                        @Override
+                        public void onError(Throwable e) {
+                            Log.e(TAG, "onError: ", e);
+                        }
+
+                        @Override
+                        public void onComplete() {
+                            Log.d(TAG, "onComplete: called.");
+                        }
+                    });
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
