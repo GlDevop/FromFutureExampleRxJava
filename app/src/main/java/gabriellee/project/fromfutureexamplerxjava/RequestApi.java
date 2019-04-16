bcfcb7741e7fcff62e6ac82a726dc5dd9d190f68
GabeLeeData <gabe95sk@gmail.com>
2019-04-16 19:08:46 -0400

    Initial commit

---
+++ app/src/main/java/gabriellee/project/fromfutureexamplerxjava/RequestApi.java
@@ -0,0 +1,12 @@
+package gabriellee.project.fromfutureexamplerxjava;
+
+import io.reactivex.Observable;
+import okhttp3.ResponseBody;
+import retrofit2.http.GET;
+
+public interface RequestApi {
+
+    @GET("todos/1")
+    Observable<ResponseBody> makeObservableQuery();
+
+}
